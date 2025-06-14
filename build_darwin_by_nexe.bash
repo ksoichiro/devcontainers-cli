14b51cf2c80aef4e206e3c71540885201afa6cfa
Soichiro Kashima <soichiro.kashima@gmail.com>
2025-06-14 16:51:08 +0900

    Fix parameter

--- build_darwin_by_nexe.bash
+++ build_darwin_by_nexe.bash
@@ -35,5 +35,5 @@ yarn
 yarn compile-prod
 
 # create single executable binary by nexe
-npx -y nexe --loglevel="verbose" -i ./devcontainer.js --make="-j3" -r="scripts/updateUID.Dockerfile" -t ${SUFFIX} -b -c "['--dest-cpu=arm64,x64']" -o ${BIN_PATH}
+npx -y nexe --loglevel="verbose" -i ./devcontainer.js --make="-j3" -r="scripts/updateUID.Dockerfile" -t ${SUFFIX} -b -c "--dest-cpu=arm64,x64" -o ${BIN_PATH}
 
